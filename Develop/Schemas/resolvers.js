335cba10260345af568a247957625d1b18584ab3
AntonioHockman <150075660+AntonioHockman@users.noreply.github.com>
2024-08-12 19:56:16 -0500

    added typeDef, and some resolver code

--- Develop/Schemas/resolvers.js
+++ Develop/Schemas/resolvers.js
@@ -1,18 +1,53 @@
 
 // import user model
+const userController = require('../server/controllers/user-controller');
 const { User } = require('../server/models');
+const { AuthenticationError } = require('../server/utils/auth');
 // import sign token function from auth
-// const { signToken } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
       user: async () => {
-        return await User.find({});
-      }
+        return await User.find();
+      },
+      user: async (parent,{userId}) => {return User.findOne({_id:userId});
     },
-
-  };
+    me: async ( parent, args, context) => {
+      if (context.user) {
+        return User.findOne ({ _id: context.user._id});
+      }
+      throw AuthenticationError;
+    },
+    },
   
+  
+  Mutation :{ 
+    addUser: async (parent,{ name,email,password}) =>  {
+      const profile = await User.create({ name, email, password });
+      const token = signToken(User);
+      return { token, profile };
+    },
+    login: async (parent, {email,password}) => {
+      const user = await User.findOne({ email });
+
+      if (!userController) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+
+
+};
   module.exports = resolvers;
 
   //log in user, saving book, deleting book, need mutation here 
--- Develop/Schemas/typeDefs.js
+++ Develop/Schemas/typeDefs.js
@@ -1,13 +1,43 @@
 const typeDefs = `
-  type User {
-    _id: ID
-    name: String
-   //username email add here 
+  type Query{
+    me: User
   }
 
-  type Query {
-    users: [User]
+  input SavedBookInput{
+  author:[String!]
+  description: String!
+  title: String!
+  image: String!
+  link: String!
+
   }
+  type Mutation {
+  login(email!, password!):Auth
+    addUser(username: String!, email: String!): Auth
+    addSavedBook(input SavedBookInput!): User
+    removeBook(bookId!):User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+    type Book{
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+    }
+
+    type Auth{
+    token:String
+    user: User
+    }
+
 `;
 
 module.exports = typeDefs;
